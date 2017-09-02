54662ba00a1f08e7e89bb98673107fd7907d1a15
Alex Chao <alex.chao@stackcommerce.com>
2017-09-02 11:50:06 -0700

    Define static assets path; setup homepage jumbotron

--- workforla/settings.py
+++ workforla/settings.py
@@ -132,6 +132,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Simplified static file serving.
