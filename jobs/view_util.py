d148252c3f58bfd1ac810d6d0959bf29e8a67e58
Alex Chao <alex.chao@stackcommerce.com>
2017-09-02 19:39:33 -0700

    Flesh out job details some more

--- jobs/view_util.py
+++ jobs/view_util.py
@@ -8,7 +8,10 @@ JobDetails = namedtuple(
     ['title',
      'salary_low',
      'salary_high',
-     'description'])
+     'description',
+     'responsibilities',
+     'qualifications',
+     'exam_notes'])
 
 
 def make_job_details(job_class):
@@ -16,4 +19,7 @@ def make_job_details(job_class):
         title=job_class.title,
         description=job_class.description,
         salary_low=util.format_currency(job_class.salary_low),
-        salary_high=util.format_currency(job_class.salary_high))
+        salary_high=util.format_currency(job_class.salary_high),
+        responsibilities=job_class.responsibilities,
+        qualifications=job_class.qualifications,
+        exam_notes=job_class.exam_notes)
