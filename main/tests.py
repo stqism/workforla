f4082ca1b7d8178094265ee40a0906a853be1409
Alex Chao <alex.chao@stackcommerce.com>
2017-09-02 18:37:05 -0700

    Test homepage

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,14 @@
+from django.test import Client
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomepageTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_homepage_loads(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Work For LA',
+                      response.content.decode(response.charset))
--- workforla/urls.py
+++ workforla/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('main.urls')),
+    url(r'', include('main.urls')),
     url(r'^jobs/', include('jobs.urls')),
     url(r'^admin/', admin.site.urls),
 ]
