22a1c4a00080955f475e492c5ceca48db22a832b
Sean <sean@tox.im>
2017-11-09 01:11:20 -0800

    New frontend test

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.homepage, name='homepage'),
+    url(r'^whyla?$', views.whyla, name='whyla'),
+    url(r'^aboutus?$', views.aboutus, name='aboutus'),
+    url(r'^process?$', views.process, name='process'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'main/homepage.html', {})
+
+def whyla(request):
+    return render(request, 'main/whyla.html', {})
+
+def process(request):
+    return render(request, 'main/process.html', {})
+
+def aboutus(request):
+    return render(request, 'main/aboutus.html', {})
